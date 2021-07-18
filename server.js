f71be70d2fafc31139c81be8234dc1e2aec89356
Noor Alhomsi <nalhomsi@gmail.com>
2021-07-18 13:42:40 -0400

    tables

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,10 +8,29 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Ledger12345!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
